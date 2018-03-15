14998bbf4fa09c2f0097e16991d70d0b7e128537
Luis Sanchez <luis@logicaldesign.pe>
2018-03-14 20:36:43 -0500

    updated

--- test.php
+++ test.php
@@ -3,6 +3,9 @@
   if ( file_exists( $argv[1] ) ) {
     $fileContent = file_get_contents($argv[1]);
     $post = array('document' => base64_encode( $fileContent ) );
+    if ( isset($argv[2]) &&  $argv[2] ) {
+      $post['type'] = $argv[2];
+    }
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, 'http://factura.logicaldesign.pe/api/send-document');
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
