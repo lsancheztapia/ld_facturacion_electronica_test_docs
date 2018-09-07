8dbfc6422087095dcad226ec7949ea3c25557718
Luis Sanchez <luis@logicaldesign.pe>
2018-09-06 21:55:58 -0500

    fix send.php for working in windows

--- send.php
+++ send.php
@@ -14,10 +14,19 @@
 //	 $post['batch'] = 'yes';
 
     $ch = curl_init();
+
+    // disabling SSL check
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+
     curl_setopt($ch, CURLOPT_URL, 'https://prueba-fact.logicaldesign.pe/api/send-document');
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post));
     $response = curl_exec($ch);
+    if ( $response === false ) {
+      print_r( curl_error($ch) );
+      die('');
+    }
     $responseArray = json_decode($response);
     if ( $responseArray  ) {
       print_r ($responseArray);
